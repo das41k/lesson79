c48bbebb5026ea2f9399e74d399bf37c7395053c
das41k <148576114+das41k@users.noreply.github.com>
2023-11-03 22:21:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const age = 18
+if (age >= 18) {
+  console.log("Вы совершеннолетний")
+} else {
+  console.log("Вы несовершеннолетний")
+}
+(age >= 18) ? console.log("Вы совершеннолетний"):console.log("Вы несовершеннолетний")
